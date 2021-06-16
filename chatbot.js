7d262bea9cce9baaa47a78c3ddb7a7ef55fe6888
Konrad Sarna <konrad.sarna@o2.pl>
2021-06-16 11:27:35 +0200

    add lowercase

--- chatbot.js
+++ chatbot.js
@@ -16,30 +16,30 @@ function bot() {
     setTimeout(timedQuestion, 2000);									// output next question after 2sec delay
     }
 //Questions and responses
-    else if (questionNum == 1 & input === "Yes" || input === "Sure") {
+    else if (questionNum == 1 & input === "Yes" || input === "Sure" || input === "yes" || input === "sure") {
     output.innerHTML = 'Alright. Then here you go, press the button below and look at random memes from Reddit. Enjoy! :)';
     document.getElementById("button").removeAttribute("hidden");
     document.getElementById("input").value = "";	      	
 
     }   
-    else if (questionNum == 1 & input === "No" || input === "Nope") {
+    else if (questionNum == 1 & input === "No" || input === "Nope" || input === "no" || input === "nope") {
     output.innerHTML = 'Well, I\'m sad now, because that\'s mostly what I do for now :(. If you still want to look at memes, just type \'Ok\'';
     document.getElementById("input").value ="";
     }
-    else if (questionNum == 2 & input === "Alright, show me some memes" || input === "Ok" || input === "Show memes") {
+    else if (questionNum == 2 & input === "Alright, show me some memes" || input === "Ok" || input === "Show memes" || input === "ok" || input === "show memes" || input === "alright, show me some memes") {
       output.innerHTML = 'Then here, click on the button below and enjoy the memes!';
       document.getElementById("button").removeAttribute("hidden");
       document.getElementById("input").value ="";
     }
-    else if (questionNum == 1 & input === "You're stupid" || input === "Fudge off" || input === "I hate you") {
+    else if (questionNum == 1 & input === "You're stupid" || input === "Fudge off" || input === "I hate you" || input === "you're stupid" || input === "fudge off") {
       output.innerHTML = "Please don't be mean... I just want you to have a good time, y'know? :( Wanna look at some memes?";
       document.getElementById("input").value ="";
     }
-    else if (questionNum == 1 & input === "I love you" || input === "You're the best" || input === "You're awesome") {
+    else if (questionNum == 1 & input === "I love you" || input === "You're the best" || input === "You're awesome" || input === "you're the best" || input === "you're awesome") {
       output.innerHTML = "Awww, thank you. You are cool too! Want to watch some memes?";
       document.getElementById("input").value ="";
     }
-    else if (questionNum == 2 || questionNum == 3  & input === "Thank you" || input === "Awesome") {
+    else if (questionNum == 2 || questionNum == 3  & input === "Thank you" || input === "Awesome" || input === "awesome" || input === "thank you") {
       output.innerHTML = "No problem. Glad I could help. Enjoy!";
       document.getElementById("input").value ="";
 }}
